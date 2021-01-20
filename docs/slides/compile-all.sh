f202623e55d56d4087d6bba5a11eb6ecf6e7f3a9
Patrick Mineault <patrick.mineault@gmail.com>
2021-01-20 14:07:36 -0500

    Move to make

--- docs/slides/compile-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pandoc -t beamer -s 01-intro.md -o 01-intro.pdf -H preamble.tex
-pandoc -t beamer -s 02-decouple.md -o 02-decouple.pdf -H preamble.tex
-pandoc -t beamer -s 03-testing.md -o 03-testing.pdf -H preamble.tex
-pandoc -t beamer -s 04-docs.md -o 04-docs.pdf -H preamble.tex
-pandoc -t beamer -s 05-social.md -o 05-social.pdf -H preamble.tex
